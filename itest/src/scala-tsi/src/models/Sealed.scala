d5daf79260db7048fd84495a56423a099df7a65b
Scala Steward <scala_steward@virtuslab.com>
2024-01-14 17:53:51 +0000

    Reformat with scalafmt 3.7.17
    Executed command: scalafmt --non-interactive

--- itest/src/scala-tsi/src/models/Sealed.scala
+++ itest/src/scala-tsi/src/models/Sealed.scala
@@ -2,4 +2,4 @@ package models
 
 sealed trait Sealed
 case class SealedOption1(foo: String) extends Sealed
-case class SealedOption2(bar: Int)    extends Sealed
+case class SealedOption2(bar: Int) extends Sealed
