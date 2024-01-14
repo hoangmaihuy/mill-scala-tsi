e6c5019191c0114db092c7266db5e5845f779206
Scala Steward <scala_steward@virtuslab.com>
2024-01-14 17:53:51 +0000

    Reformat with scalafmt 3.7.17
    Executed command: scalafmt --non-interactive

--- itest/src/scala-tsi/src/models/CaseClass.scala
+++ itest/src/scala-tsi/src/models/CaseClass.scala
@@ -3,7 +3,7 @@ package models
 case class MyCaseClass(nested: Person, bool: Boolean)
 
 case class GenericCaseClass(
-    optional: Option[String],
-    emails: Seq[Email],
-    mapping: Map[String, Person]
+  optional: Option[String],
+  emails: Seq[Email],
+  mapping: Map[String, Person]
 )
--- itest/src/scala-tsi/src/models/Function.scala
+++ itest/src/scala-tsi/src/models/Function.scala
@@ -11,9 +11,12 @@ trait GreetFunction extends ((String, Int) => String) {
   // TODO: Can we extract the parameter names from the function to export?
   override def apply(name: String, age: Int): String
 }
+
 object GreetFunction {
+
   // TODO: Infer this automatically
   implicit val tsType: TSNamedType[GreetFunction] = TSNamedType(
     TSFunctionNamed("GreetFunction", TSType.get[(String, Int) => String].get.asInstanceOf[TSFunction])
   )
+
 }
